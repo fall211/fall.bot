bcf26283c621b51f1dd75ea697565de05bea94f1
fall211 <94519579+fall211@users.noreply.github.com>
2023-03-20 11:18:02 -0700

    improved start server bash

---
+++ start_server.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+
+command_master=""
+command_caves=""
+cluster_name="$1"
+
+if [ $2 = "True" ]
+then
+	command_master+="bash run_custom_server.sh $cluster_name Master True^M"
+	command_caves+="bash run_custom_server.sh $cluster_name Caves True^M"
+else
+	command_master+="bash run_custom_server.sh $cluster_name Master False^M"
+	command_caves+="bash run_custom_server.sh $cluster_name Caves False^M"
+fi
+
+screen -S s -p 0 -X stuff "$commmand_master"
+echo "starting master shard"
+sleep 10
+screen -S c -p 0 -X stuff "$commmand_caves"
+echo "starting cave shard"
+sleep 10
+echo "both servers started"
+
