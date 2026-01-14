d400372526e5beb18e96362994db9a2b75431e65
Tuukka Virtanen <tvv@Tuukkas-MacBook-Air.local>
2026-01-14 13:32:26 -0800

    migrate cluster selection

--- cogs/server_control.py
+++ cogs/server_control.py
@@ -3,7 +3,72 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from discord.ui import View, Button
+from utils import helper_functions as hf
 import asyncio
+
+class SelectionView(discord.ui.View):
+    def __init__(self, bot):
+        super().__init__(timeout=None)
+        self.bot = bot
+        self.select_branch = discord.ui.Select(
+            placeholder="Select Branch",
+            options=[
+                discord.SelectOption(label="Main", value="main", emoji="🌲"),
+                discord.SelectOption(label="Beta", value="beta", emoji="🔄"),
+                discord.SelectOption(label="Cancel", value="Cancel", emoji="❌"),
+            ],
+            row=0,
+            custom_id="branch",
+        )
+
+        self.select_branch.callback = self.sel_branch
+        self.add_item(self.select_branch)
+
+    async def sel_branch(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        if self.select_branch.values[0] == "Cancel":
+            self.remove_item(self.select_branch)
+            self.stop()
+            await interaction.edit_original_response(view=None, content="Cancelled.")
+            await asyncio.sleep(5)
+            await interaction.delete_original_response()
+            return
+        print(
+            str(interaction.user)
+            + " changed the branch to "
+            + self.select_branch.values[0]
+        )
+
+        self.bot.state.is_beta = True if self.select_branch.values[0] == "beta" else False
+        self.remove_item(self.select_branch)
+        self.create_cluster_selection(self.bot.state.is_beta)
+        self.add_item(self.select_cluster)
+        await interaction.edit_original_response(view=self)
+
+    def create_cluster_selection(self, is_beta_server):
+        self.select_cluster = discord.ui.Select(
+            placeholder="Select Cluster",
+            options=hf.get_cluster_options(is_beta_server),
+            row=0,
+            custom_id="cluster",
+        )
+
+        self.select_cluster.callback = self.sel_cluster
+
+    async def sel_cluster(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        print(
+            str(interaction.user)
+            + " changed the cluster to "
+            + self.select_cluster.values[0]
+        )
+        self.state.current_cluster = self.select_cluster.values[0]
+        self.remove_item(self.select_cluster)
+        self.stop()
+        branch = "Beta" if self.bot.state.is_beta else "Main"
+        await interaction.edit_original_response(
+            view=self, content=f"Changed cluster to {self.state.current_cluster} on {branch} Branch."
+        )
 
 class PanelMenu(View):
     def __init__(self, bot):
@@ -31,18 +96,22 @@ class PanelMenu(View):
         await self.bot.shard_manager.restart_world()
         await interaction.followup.send("Restarting server...", ephemeral=True)
 
-    @discord.ui.select(
-        placeholder="Select a Cluster",
-        options=[
-            discord.SelectOption(label="Option 1", value="option_1"),
-            discord.SelectOption(label="Option 2", value="option_2"),
-            discord.SelectOption(label="Option 3", value="option_3"),
-        ],
+    @discord.ui.button(
+        label="Change Cluster",
+        style=discord.ButtonStyle.grey,
         row=2,
-        custom_id="change_branch"
+        custom_id="change_branch",
+        emoji="🔄",
     )
-    async def select(self, interaction: discord.Interaction, select: discord.ui.Select):
-        await interaction.response.send_message(f"You selected {select.values[0]}", ephemeral=True)
+    async def change_branch(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        print(str(interaction.user) + " started a branch/cluster change.")
+        text = f"Currently accessing {self.bot.state.current_cluster} on the {'Beta' if self.bot.state.is_beta else 'Main'} Branch."
+        await interaction.response.send_message(
+            content=text, view=SelectionView(self.bot), ephemeral=True
+        )
+
 
 class ServerControlCog(commands.Cog):
     def __init__(self, bot):
