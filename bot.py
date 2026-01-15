b66a98f1cc555454460193e18c507cc3a3d5aa23
Tuukka Virtanen <tvv@Tuukkas-MacBook-Air.local>
2026-01-14 21:32:17 -0800

    fix

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ class FallBot(commands.Bot):
         await self.load_extension("cogs.admin_commands")
         await self.load_extension("cogs.chat_relay")
 
-        await bot.tree.sync(guild=discord.Object(id=CURRENT_SERVER_ID))
+        await self.tree.sync(guild=discord.Object(id=CURRENT_SERVER_ID))
 
     async def on_ready(self):
         print(f"Logged in as {self.user}")
@@ -29,7 +29,7 @@ class FallBot(commands.Bot):
             if guild.id not in ALLOWED_SERVERS:
                 await guild.leave()
 
-        await bot.change_presence(
+        await self.change_presence(
             activity=discord.Activity(
                 name="user commands", type=discord.ActivityType.listening
             )
